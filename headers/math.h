bf7e7cfc4f4b512a4530d5023e6523c427b6a492
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-05-11 18:48:59 -0500

    Update math.h

--- headers/math.h
+++ headers/math.h
@@ -6,6 +6,6 @@ double fabs(double a){return a<0?-a:a;}
 float fabsf(float a){return a<0?-a:a;}
 long double fabsl(long double a){return a<0?-a:a;}
 
-float roundf(float k){float r=0x1p-149;return r*k/r;}
-double round(double k){double r=0x1p-1074;return r*k/r;}
-long double roundl(long double k){long double r=k>0?0x1p63:-0x1p63;return r+k-r;}
+float roundf(float k){float r=0x1p-149;return k==0.5?1:r*k/r;}
+double round(double k){double r=0x1p-1074;return k==0.5?1:r*k/r;}
+long double roundl(long double k){long double r=k>0?0x1p63:-0x1p63;return k==0.5?1:r+k-r;}
