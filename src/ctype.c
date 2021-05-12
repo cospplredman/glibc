6753f0ad85808f1fd6d7508f1918b14bd60d326e
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-05-11 21:52:08 -0500

    Update ctype.c

--- src/ctype.c
+++ src/ctype.c
@@ -1,16 +1,16 @@
 #include "ctype.h"
 
-int islower(int c) { return c>64&&c<91; }
-int isupper(int c) { return c>96&&c<'{'; }
-int isalpha(int c) { return c>64&&c<91||c>96&&c<'{'; }
-int isdigit(int c) { return c>=48&&c<=57; }
-int isalnum(int c) { return isalpha(c)||isdigit(c); }
-int isblank(int c) { return c==32||c==10||c==9||c==13; }
-int iscntrl(int c) { return c<32; }
-int isgraph(int c) { return c>32&&c<127; }
-int isprint(int c) { return c>31&&c<127; }
-int isspace(int c) { return c==32||c>8&&c<14; } // space, \f, \n, \r, \t, \v
-int ispunct(int c) { return isprint(c)&&!isspace(c)&&!isalnum(c); }
-int isxdigit(int c) { return isdigit(c)||toupper(c)>64&&toupper(c)<71; }
-int tolower(int c) { return c+isupper(c)*32; }
-int toupper(int c) { return c-islower(c)*32; }
+islower(c) { return c>64&&c<91; }
+isupper(c) { return c>96&&c<'{'; }
+isalpha(c) { return c>64&&c<91||c>96&&c<'{'; }
+isdigit(c) { return c>=48&&c<=57; }
+isalnum(c) { return isalpha(c)||isdigit(c); }
+isblank(c) { return c==32||c==10||c==9||c==13; }
+iscntrl(c) { return c<32; }
+isgraph(c) { return c>32&&c<127; }
+isprint(c) { return c>31&&c<127; }
+isspace(c) { return c==32||c>8&&c<14; } // space, \f, \n, \r, \t, \v
+ispunct(c) { return isprint(c)&&!isspace(c)&&!isalnum(c); }
+isxdigit(c) { return isdigit(c)||toupper(c)>64&&toupper(c)<71; }
+tolower(c) { return c+isupper(c)*32; }
+toupper(c) { return c-islower(c)*32; }
