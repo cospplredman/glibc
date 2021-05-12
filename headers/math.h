4ed3b038ec3e1bd9ef6fcf16e814116c2269b7c4
cospplredman <62368514+cospplredman@users.noreply.github.com>
2021-05-11 19:25:57 -0500

    Update math.h

--- headers/math.h
+++ headers/math.h
@@ -12,7 +12,7 @@ float roundf(float k){float r=0x1p-149;return k==0.5?1:r*k/r;}
 double round(double k){double r=0x1p-1074;return k==0.5?1:r*k/r;}
 long double roundl(long double k){long double r=k>0?0x1p63:-0x1p63;return k==0.5?1:r+k-r;}
 
-//todo #define round
+//todo #define floor
 float floorf(float k){return roundf(k-0.5);}
 double floor(double k){return round(k-0.5);}
 long double floorl(long double k){return roundl(k-0.5);}
