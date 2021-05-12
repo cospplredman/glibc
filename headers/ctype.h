d136fe3b860f69b68cad1ee09f0e08c8b840566a
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-05-11 21:03:25 -0600

    prototypes not needed at all

--- headers/ctype.h
+++ headers/ctype.h
@@ -1,19 +1,6 @@
 #ifndef CTYPE_H
 #define CTYPE_H
 
-islower(c);
-isupper(c);
-isalpha(c);
-isdigit(c);
-isalnum(c);
-isblank(c);
-iscntrl(c);
-isgraph(c);
-isprint(c);
-isspace(c);
-ispunct(c);
-isxdigit(c);
-tolower(c);
-toupper(c);
+// prototypes not needed at all - implicit int
 
 #endif
